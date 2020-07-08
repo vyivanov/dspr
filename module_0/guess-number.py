7a025a0aa2f66c99fd32fc9067d5ddb722c40633
Vladimir Ivanov <inbox@vova-ivanov.info>
2020-07-08 23:54:30 +0300

    Added docs and cosmetics

--- module_0/guess-number.py
+++ module_0/guess-number.py
@@ -3,28 +3,52 @@
 import numpy
 
 
-def predict(number, start, end):
+def binary_search(number, start, end):
+    """
+    Searches a number in continuous array.
+
+    Arguments:
+        number (int) - number to be found in the array
+        start  (int) - first number of the array
+        end    (int) - latest number of the array
+
+    Returns:
+        amount of attempts were used
+    """
     assert (start < end)
     assert (start <= number) and (number <= end)
 
-    counter = 0
+    attempts = 0
 
     while True:
-        counter = counter + 1
         guess = int((start + end) / 2)
+        attempts = attempts + 1
+
         if guess < number:
             start = guess + 1
         elif guess > number:
             end = guess - 1
-        else:
+        elif guess == number:
             break
 
-    return counter
+    return attempts
 
 
-def guess_game(predictor, start=1, end=100):
+def guess_game(predictor, start=1, end=100, rounds=10**3):
+    """
+    Number prediction game.
+
+    Arguments:
+        predictor (func) - prediction algorithm
+        start     (int)  - first  number of the range to predict from
+        end       (int)  - latest number of the range to predict from
+        rounds    (int)  - amount of game rounds
+
+    Returns:
+        mean of attempts to predict the number
+    """
     guesses = list()
-    randoms = numpy.random.randint(start, end + 1, size=10**5)
+    randoms = numpy.random.randint(start, end + 1, size=rounds)
 
     for number in randoms:
         guesses.append(predictor(number, start, end))
@@ -35,5 +59,7 @@ def guess_game(predictor, start=1, end=100):
 
 
 if __name__ == "__main__":
-    quality = int(guess_game(predict))
+    quality = \
+        int(guess_game(binary_search))
+
     print("Guess rate is {} attempts".format(quality))
